9cccce6bf9d959ec459a5fb4202c40cc57a05860
Victoria Ceccaroni <vceccaro@e3r5p6.42roma.it>
2022-02-14 18:37:50 +0100

    working on new module

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -12,6 +12,10 @@ Bureaucrat::~Bureaucrat()
 
 Bureaucrat::Bureaucrat(const std::string name, int grade)
 {
+	if (grade > 150)
+		throw  GradeTooHighException();
+	else if (grade < 1)
+		throw GradeTooLowException();
     this->name = name;
     this->grade = grade;
     std::cout << "Copy constructor called for Bureaucrat " << name << " with grade " << grade << std::endl;
@@ -27,3 +31,35 @@ int Bureaucrat::getGrade()
     return (this->grade);
 }
 
+void	Bureaucrat::increment(int num)
+{
+	if (getGrade() + num > 150)
+		throw GradeTooHighExeption();
+	else {
+		grade += num;
+		std::cout << this->name << " lowered grade to " << this->getGrade() << std::endl;
+	}
+}
+
+void	Bureaucrat::decrement(int num)
+{
+	if (getGrade() - num < 1)
+		throw GradeTooLowExeption;
+	else{
+		grade -= num;
+		std::cout << this->name << |" increased grade to " << this->getGrade() << std::endl;
+	}
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return "GRADE IS TOO HIGH";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return "GRADE IS TOO LOW";
+}
+
+std::ostream& operator<<(std::ostream & os, Bureaucrat & i)
+{
+	return(os << i.getName() + ", grade: " << i.getGrade());
+}
