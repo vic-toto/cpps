8ed217add6a5a04d88b5bf3156f9f93f7c6bc689
vic-toto <victoriaceccaroni@gmail.com>
2022-03-04 16:25:22 +0100

    finished ex00

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -5,13 +5,40 @@
 #include <stdio.h>
 #include <cmath>
 
+void	print(int i, float f, double d, char c)
+{
+	double dex;
+
+	std::cout << "Int: ";
+	if (d > 2146473647 || d < -2146473648 || isnan(d) || isinf(d))
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << i << std::endl;
+	std::cout << "Float: ";
+	if ((d < __FLT_MIN__ || d > __FLT_MAX__) && ((!isnan(f) && !isinf(f)) && f))
+		std::cout << "impossible" << std::endl;
+	else if (!modf(f, &dex) && !isnan(f) && !isinf(f))
+		std::cout << std::setprecision(7) << f << ".0f" << std::endl;
+	else
+		std::cout << std::setprecision(7) << f <<  "f" << std::endl;
+	std::cout << "Double: ";
+	if (!modf(d, &dex) && !isnan(d) && !isinf(d))
+		std::cout << std::setprecision(16) << d << ".0" << std::endl;
+	else
+		std::cout << std::setprecision(16) << d << std::endl;
+	std::cout << "Char: ";
+	if (isprint(c))
+		std::cout << c << std::endl;
+	else
+		std::cout << "not displayable" << std::endl;
+}
 
 bool	isInt(std::string arg)
 {
-	if (arg[0] != '-' && arg[0] != '+' && !isdigit(arg[0])
-		|| ((arg[0] == '-') || arg[0] == '+') && arg.length() == 1)
+	if ((arg[0] != '-' && arg[0] != '+' && !isdigit(arg[0]))
+		|| (((arg[0] == '-') || arg[0] == '+') && arg.length() == 1))
 			return false;
-	int i = 1;
+	size_t i = 1;
 	for ( ; i < arg.length() && isdigit(arg[i]); i++)
 		continue ;
 	if (i == arg.length())
@@ -27,7 +54,7 @@ bool	isFloat(std::string arg)
 	if (arg[0] != '-' && arg[0] != '+' && !isdigit(arg[0]))
 		return false ;
 	int f = 0, p = 0;
-	for (int i = 1; i < arg.length(); i++)
+	for (size_t i = 1; i < arg.length(); i++)
 	{
 		if (arg[i] == '.')
 			p++;
@@ -49,7 +76,7 @@ bool	isDouble(std::string arg)
 	if (arg[0] != '-' && arg[0] != '+' && !isdigit(arg[0]))
 		return false ;
 	int p = 0;
-	for (int i = 1; i < arg.length(); i++)
+	for (size_t i = 1; i < arg.length(); i++)
 	{
 		if (arg[i] == '.')
 			p++;
@@ -83,6 +110,66 @@ int	getType(std::string str)
 		return (4);
 }
 
+void	doChar(std::string arg)
+{
+	char c = 0;
+	float f = 0;
+	double d = 0;
+	int i = 0;
+
+	c = arg[0];
+	i = static_cast<int>(c);
+	f = static_cast<float>(c);
+	d = static_cast<double>(c);
+	std::cout << "CHAR" << std::endl;
+	print(i, f, d, c);
+}
+
+void	doDouble(std::string arg)
+{
+	char c = 0;
+	float f = 0;
+	double d = 0;
+	int i = 0;
+
+	d = atof(arg.c_str());
+	i = static_cast<int>(d);
+	f = static_cast<float>(d);
+	c = static_cast<char>(d);
+	std::cout << "DOUBLE" << std::endl;
+	print(i, f, d, c);
+}
+
+void	doFloat(std::string arg)
+{
+	char c = 0;
+	float f = 0;
+	double d = 0;
+	int i = 0;
+
+	d = atof(arg.c_str());
+	i = static_cast<int>(d);
+	f = static_cast<float>(d);
+	c = static_cast<char>(d);
+	std::cout << "FLOAT" << std::endl;
+	print(i, f, d, c);
+}
+
+void	doInt(std::string arg)
+{
+	char c = 0;
+	float f = 0;
+	double d = 0;
+	int i = 0;
+
+	d = atof(arg.c_str());
+	i = static_cast<int>(d);
+	f = static_cast<float>(d);
+	c = static_cast<char>(d);
+	std::cout << "INT" << std::endl;
+	print(i, f, d, c);
+}
+
 void	doStuff(std::string arg)
 {
 	int i = getType(arg);
@@ -98,7 +185,7 @@ void	doStuff(std::string arg)
 			doDouble(arg);
 			break ;
 	case 3:
-			doStr(arg);
+			doChar(arg);
 			break ;
 	case 4:
 			std::cout << "Invalid input" << std::endl;
